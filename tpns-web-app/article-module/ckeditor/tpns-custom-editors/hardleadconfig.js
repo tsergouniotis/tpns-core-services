5636416e519aff9ee8aa1e8cb0c4ea14e6742115
nicohaitas <nicohaitas@gmail.com>
2016-09-06 14:59:04 +0300

    Updated the configuration and is now able to accept multiple editors each with their own configuration
    Signed-off-by: nicohaitas <nicohaitas@gmail.com>

---
+++ tpns-web-app/article-module/ckeditor/tpns-custom-editors/hardleadconfig.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+// This is the config file for the Hard Lead WYSIWYG Editor
+CKEDITOR.editorConfig = function( config ) {
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,horizontalrule,htmlwriter,wysiwygarea,image,indent,indentlist,fakeobjects,link,list,magicline,maximize,pastetext,pastefromword,removeformat,showborders,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc,autogrow,lineutils,widget,notification,notificationaggregator,embedbase,embedsemantic';
+	config.skin = 'minimalist';
+	// %REMOVE_END%
+
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Subscript,Superscript,RemoveFormat,Strike,NumberedList,Outdent,Indent,Blockquote,Cut,Copy,Anchor,Maximize,Source,Image,EmbedSemantic,Table,HorizontalRule,Styles,About,Format';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+};
