2fa018aa66f291752912b60015add84e525a3502
nicohaitas <nicohaitas@gmail.com>
2016-09-06 18:12:37 +0300

    Added CKEditor config JS per instance and wordcount feature
    Signed-off-by: nicohaitas <nicohaitas@gmail.com>

--- tpns-web-app/article-module/ckeditor/tpns-custom-editors/hardleadconfig.js
+++ tpns-web-app/article-module/ckeditor/tpns-custom-editors/hardleadconfig.js
@@ -3,7 +3,7 @@
 CKEDITOR.editorConfig = function( config ) {
 	// %REMOVE_START%
 	// The configuration options below are needed when running CKEditor from source files.
-	config.plugins = 'dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,horizontalrule,htmlwriter,wysiwygarea,image,indent,indentlist,fakeobjects,link,list,magicline,maximize,pastetext,pastefromword,removeformat,showborders,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc,autogrow,lineutils,widget,notification,notificationaggregator,embedbase,embedsemantic';
+	config.plugins = 'dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,horizontalrule,htmlwriter,wysiwygarea,image,indent,indentlist,fakeobjects,link,list,magicline,maximize,pastetext,pastefromword,removeformat,showborders,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc,autogrow,lineutils,widget,notification,notificationaggregator,embedbase,embedsemantic,wordcount';
 	config.skin = 'minimalist';
 	// %REMOVE_END%
 
@@ -37,4 +37,35 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	
+	// Word Count Settings
+	config.wordcount = {
+		// Whether or not you want to show the Paragraphs Count
+		showParagraphs: false,
+		// Whether or not you want to show the Word Count
+		showWordCount: false,
+		// Whether or not you want to show the Char Count
+		showCharCount: true,
+		// Whether or not you want to count Spaces as Chars
+		countSpacesAsChars: true,
+		// Whether or not to include Html chars in the Char Count
+		countHTML: false,
+		// Maximum allowed Word Count, -1 is default for unlimited
+		maxWordCount: -1,
+		// Maximum allowed Char Count, -1 is default for unlimited
+		maxCharCount: 150,
+		// How long to show the 'paste' warning, 0 is default for not auto-closing the notification
+		pasteWarningDuration: 0,
+		// Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
+		filter: new CKEDITOR.htmlParser.filter({
+			elements: {
+				div: function( element ) {
+					if(element.attributes.class == 'mediaembed') {
+						return false;
+					}
+				}
+			}
+		})
+	};
+	
 };
