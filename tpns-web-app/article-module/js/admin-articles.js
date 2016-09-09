fa0766372e5ceb0561c0e8ab0b04dc840b3e2228
nicohaitas <nicohaitas@gmail.com>
2016-09-09 12:19:21 +0300

    Added Billboard and Nutshell
    Signed-off-by: nicohaitas <nicohaitas@gmail.com>

--- tpns-web-app/article-module/js/admin-articles.js
+++ tpns-web-app/article-module/js/admin-articles.js
@@ -16,35 +16,6 @@
 
 $(document).ready( function() {
 	'use strict';
-	// Initialize jquery-uploader
-	// Dev Docs can be found here... http://hayageek.com/docs/jquery-upload-file.php
-	$("#kickerUploader").uploadFile(
-		{
-			url:"#",
-			fileName:"#",
-			uploadStr:"Add Image",
-			dragDrop:true,
-			maxFileCount:1,
-			multiple:false,
-			sequential:true,
-			acceptFiles:"image/*",
-			showPreview:true,
-			previewHeight: "100px",
-			previewWidth: "auto",
-			multiDragErrorStr: "You can only add one image to the Kicker",
-			dragDropStr: "<span>Drag &amp; Drop Files Here...</span>",
-			showDelete: true,
-			deleteCallback: function (data, pd) {
-			for (var i = 0; i < data.length; i++) {
-				$.post("#", {op: "delete",name: data[i]},
-				function (resp,textStatus, jqXHR) {
-					//Show Message	
-					alert("File Deleted");
-				});
-			}
-			pd.statusbar.hide(); //Your choice.
-		},
-	});
 	// Initialize CountChars in form
 	$(".new-article-textbox").countChar();
 	// Reset Articles Filter on click of the close button
@@ -93,6 +64,89 @@ $(document).ready( function() {
 		}
     });
 	
+	// Initialize jquery-uploader
+	// Dev Docs can be found here... http://hayageek.com/docs/jquery-upload-file.php
+	$("#kickerUploader").uploadFile(
+		{
+			url:"#",
+			fileName:"#",
+			uploadStr:"Add Image",
+			dragDrop:true,
+			maxFileCount:1,
+			multiple:false,
+			sequential:true,
+			acceptFiles:"image/*",
+			showPreview:true,
+			previewHeight: "100px",
+			previewWidth: "auto",
+			multiDragErrorStr: "You can only add one image to the Kicker",
+			dragDropStr: "<span>Drag &amp; Drop Files Here...</span>",
+			showDelete: true,
+			deleteCallback: function (data, pd) {
+			for (var i = 0; i < data.length; i++) {
+				$.post("#", {op: "delete",name: data[i]},
+				function (resp,textStatus, jqXHR) {
+					//Show Message	
+					alert("File Deleted");
+				});
+			}
+			pd.statusbar.hide(); //Your choice.
+		},
+	});
+	$("#billboardUploader").uploadFile(
+		{
+			url:"#",
+			fileName:"#",
+			uploadStr:"Add Image",
+			dragDrop:true,
+			maxFileCount:1,
+			multiple:false,
+			sequential:true,
+			acceptFiles:"image/*",
+			showPreview:true,
+			previewHeight: "100px",
+			previewWidth: "auto",
+			multiDragErrorStr: "You can only add one image to the Kicker",
+			dragDropStr: "<span>Drag &amp; Drop Files Here...</span>",
+			showDelete: true,
+			deleteCallback: function (data, pd) {
+			for (var i = 0; i < data.length; i++) {
+				$.post("#", {op: "delete",name: data[i]},
+				function (resp,textStatus, jqXHR) {
+					//Show Message	
+					alert("File Deleted");
+				});
+			}
+			pd.statusbar.hide(); //Your choice.
+		},
+	});
+	$("#nutshellUploader").uploadFile(
+		{
+			url:"#",
+			fileName:"#",
+			uploadStr:"Add Image",
+			dragDrop:true,
+			maxFileCount:1,
+			multiple:false,
+			sequential:true,
+			acceptFiles:"image/*",
+			showPreview:true,
+			previewHeight: "100px",
+			previewWidth: "auto",
+			multiDragErrorStr: "You can only add one image to the Kicker",
+			dragDropStr: "<span>Drag &amp; Drop Files Here...</span>",
+			showDelete: true,
+			deleteCallback: function (data, pd) {
+			for (var i = 0; i < data.length; i++) {
+				$.post("#", {op: "delete",name: data[i]},
+				function (resp,textStatus, jqXHR) {
+					//Show Message	
+					alert("File Deleted");
+				});
+			}
+			pd.statusbar.hide(); //Your choice.
+		},
+	});
 	
 	
 	
