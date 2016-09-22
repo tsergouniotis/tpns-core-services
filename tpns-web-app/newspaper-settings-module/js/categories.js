a1c99ac5565fe6b4d141075c14400beee4cde45d
nicohaitas <nicohaitas@gmail.com>
2016-09-22 20:26:32 +0300

    Categories Sort
    Added Animation to the proof of concept
    
    Signed-off-by: nicohaitas <nicohaitas@gmail.com>

--- tpns-web-app/newspaper-settings-module/js/categories.js
+++ tpns-web-app/newspaper-settings-module/js/categories.js
@@ -1,20 +1,23 @@
 $(document).ready( function() {
 	'use strict';
     $(".reorder-up, .sub-reorder-up").click(function(){
-      var $current = $(this).closest('li');
+      var $current = $(this).closest('li').fadeOut().slideUp("slow");
       var $previous = $current.prev('li');
       if($previous.length !== 0){
-        $current.insertBefore($previous);
+        $current.insertBefore($previous).fadeIn().slideDown("slow");
       }
       return false;
     });
 
     $(".reorder-down, .sub-reorder-down").click(function(){
-      var $current = $(this).closest('li');
+      var $current = $(this).closest('li').fadeOut().slideUp("slow");
       var $next = $current.next('li');
       if($next.length !== 0){
-        $current.insertAfter($next);
+        $current.insertAfter($next).fadeIn().slideDown("slow");
       }
       return false;
     });
 });
+
+
+
