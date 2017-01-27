5a3ebae4ff5c3a09db63954583f4cb2325f8aebb
nicohaitas <nicohaitas@gmail.com>
2017-01-27 16:05:51 +0200

    TPNS One - TPNS Lifestyle Magazine and responsive completed and tested
    NOTE:
    Panos if their is time lets see if we can make it 100% dynamic, by pre-measuring the images rather than resticting the user to a specific width or height or having to manually adjust the ratio in the JS
    
    Signed-off-by: nicohaitas <nicohaitas@gmail.com>

--- tpns-web-app/tpns-one/js/tpns-one.js
+++ tpns-web-app/tpns-one/js/tpns-one.js
@@ -145,6 +145,77 @@ var swiper = new Swiper('.swiper-container', {
 		});
 	}
 });
+// Lifestyle Magazine
+(function () {
+	'use strict';
+	var module = {
+		// Ratio = Image Width / Image Height
+		ratio:2,
+		init: function (id) {
+			var me = this;
+
+			// if older browser then don't run javascript
+			if (document.addEventListener) {
+				this.el = document.getElementById(id);
+				this.resize();
+				this.plugins();
+
+				// on window resize, update the plugin size
+				window.addEventListener('resize', function () {
+					var size = me.resize();
+					$(me.el).turn('size', size.width, size.height);
+				});
+			}
+		},
+		resize: function () {
+			// reset the width and height to the css defaults
+			this.el.style.width = '';
+			this.el.style.height = '';
+
+			var width = this.el.clientWidth,
+				height = Math.round(width / this.ratio),
+				padded = Math.round(document.body.clientHeight * 1);
+
+			// if the height is too big for the window, constrain it
+			if (height > padded) {
+				height = padded;
+				width = Math.round(height * this.ratio);
+			}
+
+			// set the width and height matching the aspect ratio
+			this.el.style.width = width + 'px';
+			this.el.style.height = height + 'px';
+
+			return {
+				width: width,
+				height: height
+			};
+		},
+		plugins: function () {
+			//$('.flipbook .double').scissor();
+			// run the plugin
+			$(this.el).turn({
+				//gradients: true,
+				//acceleration: true,
+				page:2
+			});
+			// hide the body overflow
+			document.body.className = 'hide-overflow';
+		}
+	};
+	$('.lifestyle-magazine .double').scissor();
+	module.init('lifestyle-magazine');
+	$('.lifestyle-magazine').bind("turning", function(event, page) {
+		if (page===1){
+			event.preventDefault();
+		}
+	});
+	$(".lifestyle-magazine").bind("start", function(event, pageObject) {
+		if (pageObject.page===1){
+			event.preventDefault();
+		}
+	});
+}());
 // Wrap every x divs with another
 $.fn.wrapMatchingDivs = function(size) {
 	'use strict';
