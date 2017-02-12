5cf5b6688a2e7b398911b44ac1e6095c1c06c59f
pzografos <pzograf@hotmail.com>
2017-02-12 21:12:55 +0200

    started trash html

---
+++ tpns-web-app/article-module/js/trash.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+// CountChars in form based on html maxlength attribute
+(function($) {
+'use strict';
+$.fn.countChar = function() {
+	return this.each(function(i, element) {
+		$(element).keyup(function updateCharCounter() {
+				var $countCharInput = $(this),
+				maxLength = parseInt($countCharInput.attr('maxlength'), 10),
+				charCount = $countCharInput.val().length,
+				$counter = $countCharInput.siblings('.limit');$counter.text(maxLength - charCount);
+			});
+		});
+	};
+}(jQuery));
+
+// Refresh Page and get fresh code from server
+$(".articles-content-refresh-button a").click(function() {
+	'use strict';
+	location.reload(true);
+});
+
+// Reset Articles Filter on click of the close button
+$(".filter-active").find("a").click(function(e) {
+	'use strict';
+	$(this).parent().parent().find(".filter-active-sub-category-container, .filter-active-first-seperator").addClass("hidden");
+	$(this).parent().parent().find(".filter-active-reporter, .filter-active-category, .filter-active-sub-category").html('');
+	$(this).parent().parent().find(".filter-active-reporter").removeClass("hidden").removeClass("reporter-active");
+	$(this).parent().parent().find(".filter-active-category").removeClass("category-active");
+	$(this).parent().parent().addClass("hidden");
+	$(this).parent().parent().parent().find(".filter-inactive").removeClass("hidden");
+	$(".admin-bi-reporter-list").find(".current").removeAttr("class", "current");
+	$(".admin-bi-reporter-list li:first-child").addClass("current").parent().parent().parent().parent().find('.selected-admin-bi-reporter-image').html($(".admin-bi-reporter-list li:first-child").find("u").clone());
+	$(".admin-bi-reporter-list li:first-child").parent().parent().parent().parent().find('.selected-admin-bi-date-filter').html($(".admin-bi-reporter-list li:first-child").find("span").html());
+	$(".admin-bi-category-list li").removeClass("current");
+	$(".admin-bi-category-list li:first-child").addClass("current").parent().parent().parent().find('.selected-admin-bi-date-filter').html($(".admin-bi-category-list li:first-child").find("span").html());
+	$('[class^="admin-bi-date-filter-selected-"]').addClass("hidden");
+	e.preventDefault();
+});
+
+// Document Ready
+$(document).ready( function() {
+	'use strict';
+
+	readCookie();
+
+	var articlesView = new TrashListView({collection: articles});
+
+	$('.allTrashTableContainer').append(articlesView.render().el.childNodes);
+
+});
